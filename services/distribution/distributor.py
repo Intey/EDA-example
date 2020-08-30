1ed1c623d29345103bebeaeddf00c69cd215f730
intey <churin.d.r@sberbank.ru>
2020-08-30 15:08:58 +0300

    [up]: distribution

--- services/distribution/distributor.py
+++ services/distribution/distributor.py
@@ -1,10 +1,16 @@
-import pika
 from os import environ
+from pydantic import BaseModel
 import json
 from uuid import uuid4
-
+import kafka
 
 REPEATS = int(environ.get("DISTRIBUTION_COUNT", "3"))
+
+
+class Task(BaseModel):
+    id: str
+    object: dict
+    type: str = "task"
 
 
 class Routes:
@@ -12,7 +18,21 @@ class Routes:
     TASK_REQUESTS = "tasks-requests"
 
 
-EXCHANGE = "ebs"
+class MQKafka:
+    def __init__(self):
+        self.producer = kafka.KafkaProducer(bootstrap_servers="localhost:9092")
+
+    def send(self, obj: dict, route: str):
+        f = self.producer.send(route, value=json.dumps(obj).encode("utf8"))
+        f.get(timeout=20)
+
+    def accept(self, callback):
+        self.consumer = kafka.KafkaConsumer(
+            Routes.OBJECTS, enable_auto_commit=False, group_id="distributor"
+        )
+        for msg in self.consumer:
+            callback(json.loads(msg.value.decode("utf-8")))
+            self.consumer.commit()
 
 
 class Distributor:
@@ -22,49 +42,20 @@ class Distributor:
     processors
     """
 
-    # exchange_name = '...'
+    def __init__(self):
+        self.mq = MQKafka()
 
-    def __init__(self, *args, **kwargs):
-        host, port = environ.get("QUEUE_URL").split(":")
+    def consume(self):
+        self.mq.accept(self.process_message)
 
-        self.conn = pika.BlockingConnection(pika.ConnectionParameters(host, port))
-        self.channel = self.conn.channel()
-
-    def consume(self, callback):
-        self.channel.exchange_declare(exchange=EXCHANGE, exchange_type="topic")
-        res = self.channel.queue_declare("distributor_consume")
-        self.channel.queue_bind(
-            exchange=EXCHANGE, queue=res.method.queue, routing_key=Routes.OBJECTS
-        )
-        self.channel.basic_consume(
-            queue=res.method.queue, on_message_callback=callback, auto_ack=True
-        )
-        self.channel.start_consuming()
-
-
-def send_to_queue(obj: dict):
-    print("send to queue obj", obj)
-    host, port = environ.get("QUEUE_URL").split(":")
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host, port))
-    channel = connection.channel()
-
-    channel.exchange_declare(exchange=EXCHANGE, exchange_type="topic")
-    channel.basic_publish(
-        exchange=EXCHANGE, routing_key=Routes.TASK_REQUESTS, body=json.dumps(obj)
-    )
-    connection.close()
-
-
-def callback(ch, method, properties, body):
-    data = json.loads(body.decode("utf8"))
-    print("Distributor got message with route", method.routing_key)
-    if method.routing_key == Routes.OBJECTS:
+    def process_message(self, data: dict):
         for i in range(REPEATS):
-            virtual = dict(id=uuid4().hex, object=data)
-            # push in channel
-            send_to_queue(virtual)
+            task = Task(id=uuid4().hex, object=data)
+            print(f"distribute object {data} as {task}")
+            self.mq.send(task.dict(), Routes.TASK_REQUESTS)
 
 
 if __name__ == "__main__":
-    consumer = Distributor()
-    consumer.consume(callback)
+    distributor = Distributor()
+    print("start consume")
+    distributor.consume()
